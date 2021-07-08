3e500f3c55ea489e6bafe836cf6503e7553424bf
Seth-Race <sethrace@gmail.com>
2021-07-08 10:46:29 -0400

    Add project files.

---
+++ EntityFrameworkCoreTutorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using EntityFrameworkCoreTutorial.Models;
+
+namespace EntityFrameworkCoreTutorial {
+    class Program {
+        static void Main(string[] args) {
+
+            var _context = new salesdbContext();
+
+            var customers = _context.Customers
+                                    .Where(c => c.City == "Cincinnati" && c.Sales >= 50000)
+                                    .OrderByDescending(c => c.Name)
+                                    .ToList();
+
+            foreach (var c in customers) {
+                Console.WriteLine($"{c.Name}|{c.City}|{c.State}|{c.Sales}");
+            }
+
+
+
+        }
+    }
+}
